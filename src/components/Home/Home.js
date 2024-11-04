5b58060751ce6f4470c7f6d12081e76e5ad15ae0
mcsl10 <marksonielloka@hotmail.com>
2024-11-03 22:39:08 -0500

    Cleaned LinkedIn link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -54,7 +54,7 @@ const Home = () => {
         <ContainerForProfileLinks>
           <TransformEffectForLinks>
             <IconLink
-              href="https://www.linkedin.com/in/mark-soniel-loka-4a9155198"
+              href="https://www.linkedin.com/in/mark-soniel-loka"
               target="_blank"
               rel="noopener noreferrer"
             >
